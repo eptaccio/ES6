9c3dd76cab0db83791d3e1107ba31e2a2ba3e0e5
Marcos Florencio <dstepbw@gmail.com>
2017-11-28 10:00:55 -0200

    Update destructuring.js

--- examples/destructuring.js
+++ examples/destructuring.js
@@ -2,12 +2,17 @@ const fullPerson = {
   name: 'Alan',
   secondName: 'Musk',
   email: 'alanzinho@hotmail.com',
-  contacts: [
-    'alan@bol.com', 'rua tapevi'
+  phones: [
+    '98756651', '98876651'
   ]
 }
 
-const {name, email} = fullPerson
+const {name, phones} = fullPerson
 
 // name -> Alan
-// email -> alanzinho@hotmail.com
+// phones -> ['98756651', '98876651']
+
+const [first, second] = phones
+
+// first -> 98756651
+// second -> 98876651
